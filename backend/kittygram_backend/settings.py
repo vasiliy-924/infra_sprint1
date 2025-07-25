a6539bd80bbc62861d46a71bcd65f012c008240a
wassiliy <vasiliy924vip@yandex.ru>
2025-07-25 18:16:38 +0000

    before review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,15 @@
+from dotenv import load_dotenv
 import os
 from pathlib import Path
 
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = False
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thunderkitty.hopto.org', '89.169.177.177', '127.0.0.1', 'localhost']  
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -90,10 +92,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend' 
 
+MEDIA_ROOT = '/var/www/kittygram/media'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -110,3 +113,4 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 
 }
+
